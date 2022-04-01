4cbd0a6a051df4cf0b6c5f4f0a0270f6055ac0b7
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-04-01 19:22:06 +0100

    commit const style guide

---
+++ lib/styleguide.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+const TextStyle display1 = TextStyle(
+  fontFamily: "WorkSans",
+  color: Colors.black,
+  fontSize: 38,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 1.2,
+);
+
+const TextStyle display2 = TextStyle(
+  fontFamily: "WorkSans",
+  color: Colors.black,
+  fontSize: 32,
+  fontWeight: FontWeight.normal,
+  letterSpacing: 1.1,
+);
+
+final TextStyle heading = TextStyle(
+  fontFamily: "WorkSans",
+  color: Colors.white.withOpacity(0.8),
+  fontSize: 34,
+  fontWeight: FontWeight.w900,
+  letterSpacing: 1.2,
+);
+
+final TextStyle subHeading = TextStyle(
+  inherit: true,
+  fontFamily: "WorkSans",
+  color: Colors.white.withOpacity(0.8),
+  fontSize: 24,
+  fontWeight: FontWeight.w500,
+);
